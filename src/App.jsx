a3d89e3db57b735c57e14b3e079da2c3777b49a0
isaacach <124728947+isaacach@users.noreply.github.com>
2023-09-14 22:28:09 -0500

    added color transition for nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import './App.css'
 import { Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import { useState } from 'react'
 
 function App() {
+  const [colorMode, setColorMode] = useState('dark-mode')
+
+  const onColorSubmit = (color) => {
+    color === 'light' ? setColorMode('light-mode') : setColorMode('dark-mode');
+  }
 
   return (
     
       <div className='app'>
-        <Navbar />
+        <Navbar colorChange={onColorSubmit} colorMode={colorMode}/>
         <Routes>
 
         </Routes>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,39 @@
-import '../styles/navbar.css'
+/* eslint-disable react/prop-types */
+import { useEffect, useRef, useState } from "react";
+import "../styles/navbar.css";
 
-export default function Navbar() {
+export default function Navbar({ colorChange, colorMode }) {
+  const [key, setKey] = useState(0);
+  const inputRef = useRef(null)
+  
+  useEffect(() => {
+    if (inputRef.current.checked == true) {
+      colorChange("light");
+    }
+    if (inputRef.current.checked == false) {
+      colorChange("dark");
+    }
+  }, [key])
+  
+
+
   return (
-    <nav>
+    <nav className={colorMode}>
       <header className="header-wrapper">
         <div className="img-wrapper">
           <img src="abstract.png" alt="brand icon" />
         </div>
         <div className="brand-wrapper">
-          <p>Useful</p>
+          <p className={colorMode}>Useful</p>
           <p>Components</p>
         </div>
       </header>
       <div className="light-dark-wrapper">
-          <input type="checkbox" />
-          <p className='dark'>Dark</p>
-          <p className='light'>Light</p>
-          <div className="slider"></div>
-        </div>
+        <input ref={inputRef} onChange={() => setKey(k => k + 1)} type="checkbox" />
+        <p className="dark">Dark</p>
+        <p className="light">Light</p>
+        <div className="slider"></div>
+      </div>
     </nav>
-  )
+  );
 }
